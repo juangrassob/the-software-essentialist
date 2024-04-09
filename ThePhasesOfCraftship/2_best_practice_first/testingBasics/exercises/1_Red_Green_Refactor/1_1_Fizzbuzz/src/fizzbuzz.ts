ce4b7237f2a576ba10c22d702bf34fd30af1a6bf
Juan Grasso <juangrassob@gmail.com>
2024-04-09 18:57:38 -0300

    [green] fizzbuzz return 'buzz' when i pass the number 10

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,7 +2,7 @@ export function fizzbuzz(n: number) {
   if (n % 3 === 0 && n % 5 === 0) {
     return "fizzbuzz";
   }
-  if (n === 5) return "buzz";
+  if (n % 5 === 0) return "buzz";
   if (n % 3 === 0) return "fizz";
   return String(n);
 }
