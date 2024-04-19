9318cc672fd0cce964387dce072c7763b53bc19d
Juan Grasso <juangrassob@gmail.com>
2024-04-18 22:49:27 -0300

    [Refactor] Implemented it.each() to condense the tests

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -12,82 +12,40 @@ describe("password validator", () => {
     expect(errors.length).toEqual(0);
   });
 
-  it("Return false and a array with the error 'missing digit' for the password 'Badpass'", () => {
-    const { validPassword, errors } =
-      PasswordValidator.checkPassword("Badpass");
+  it.each(["Badpass", "Master", "JuanTest"])(
+    "Return false and a array with the error 'missing digit' for the password '%s'",
+    (password: string) => {
+      const { validPassword, errors } =
+        PasswordValidator.checkPassword(password);
 
-    expect(validPassword).toBeFalsy();
-    expect(errors.length).toEqual(1);
-    expect(errors[0]).toEqual("missing digit");
-  });
+      expect(validPassword).toBeFalsy();
+      expect(errors.length).toEqual(1);
+      expect(errors[0]).toEqual("missing digit");
+    }
+  );
 
-  it("Return false and a array with the error 'missing digit' for the password 'Master'", () => {
-    const { validPassword, errors } = PasswordValidator.checkPassword("Master");
+  it.each(["badpass1", "master1123", "juanpassword1"])(
+    "Return false and a array with the error 'missing upper case' for the password '%s'",
+    (password: string) => {
+      const { validPassword, errors } =
+        PasswordValidator.checkPassword(password);
 
-    expect(validPassword).toBeFalsy();
-    expect(errors.length).toEqual(1);
-    expect(errors[0]).toEqual("missing digit");
-  });
+      expect(validPassword).toBeFalsy();
+      expect(errors.length).toEqual(1);
+      expect(errors[0]).toEqual("missing upper case");
+    }
+  );
 
-  it("Return false and a array with the error 'missing digit' for the password 'JuanTest'", () => {
-    const { validPassword, errors } =
-      PasswordValidator.checkPassword("JuanTest");
+  it.each(["ReallyLongPassword1", "Mom1", "1Cat"])(
+    "Return false and a array with the error 'too short or too long' for the password '%s'",
+    (password: string) => {
+      const { validPassword, errors } = PasswordValidator.checkPassword(
+        "ReallyLongPassword1"
+      );
 
-    expect(validPassword).toBeFalsy();
-    expect(errors.length).toEqual(1);
-    expect(errors[0]).toEqual("missing digit");
-  });
-
-  it("Return false and a array with the error 'missing upper case' for the password 'badpass1'", () => {
-    const { validPassword, errors } =
-      PasswordValidator.checkPassword("badpass1");
-
-    expect(validPassword).toBeFalsy();
-    expect(errors.length).toEqual(1);
-    expect(errors[0]).toEqual("missing upper case");
-  });
-
-  it("Return false and a array with the error 'missing upper case' for the password 'master1123'", () => {
-    const { validPassword, errors } =
-      PasswordValidator.checkPassword("master1123");
-
-    expect(validPassword).toBeFalsy();
-    expect(errors.length).toEqual(1);
-    expect(errors[0]).toEqual("missing upper case");
-  });
-
-  it("Return false and a array with the error 'missing upper case' for the password 'juanpassword1'", () => {
-    const { validPassword, errors } =
-      PasswordValidator.checkPassword("juanpassword1");
-
-    expect(validPassword).toBeFalsy();
-    expect(errors.length).toEqual(1);
-    expect(errors[0]).toEqual("missing upper case");
-  });
-
-  it("Return false and a array with the error 'too short or too long' for the password 'ReallyLongPassword1'", () => {
-    const { validPassword, errors } = PasswordValidator.checkPassword(
-      "ReallyLongPassword1"
-    );
-
-    expect(validPassword).toBeFalsy();
-    expect(errors.length).toEqual(1);
-    expect(errors[0]).toEqual("too short or too long");
-  });
-
-  it("Return false and a array with the error 'too short or too long' for the password 'Mom1'", () => {
-    const { validPassword, errors } = PasswordValidator.checkPassword("Mom1");
-
-    expect(validPassword).toBeFalsy();
-    expect(errors.length).toEqual(1);
-    expect(errors[0]).toEqual("too short or too long");
-  });
-
-  it("Return false and a array with the error 'too short or too long' for the password '1Cat'", () => {
-    const { validPassword, errors } = PasswordValidator.checkPassword("1Cat");
-
-    expect(validPassword).toBeFalsy();
-    expect(errors.length).toEqual(1);
-    expect(errors[0]).toEqual("too short or too long");
-  });
+      expect(validPassword).toBeFalsy();
+      expect(errors.length).toEqual(1);
+      expect(errors[0]).toEqual("too short or too long");
+    }
+  );
 });
