a74e602c61177213bfee77e345d79a8da10050db
Juan Grasso <juangrassob@gmail.com>
2024-04-23 13:16:10 -0300

    Added more test cases

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -38,6 +38,42 @@ describe("Stats calculator", () => {
         average: 4,
       },
     },
+    {
+      numbers: [0, 0, 0, 0],
+      expectedValues: {
+        minimin: 0,
+        maximun: 0,
+        numberOfElements: 4,
+        average: 0,
+      },
+    },
+    {
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      expectedValues: {
+        minimin: 1,
+        maximun: 10,
+        numberOfElements: 10,
+        average: 5.5,
+      },
+    },
+    {
+      numbers: [-99, 28, 2.2, 812, 23, 0, -2, -45],
+      expectedValues: {
+        minimin: -99,
+        maximun: 812,
+        numberOfElements: 8,
+        average: 89.9,
+      },
+    },
+    {
+      numbers: [2, 4, 21, -8, 53, 40],
+      expectedValues: {
+        minimin: -8,
+        maximun: 53,
+        numberOfElements: 6,
+        average: 18.666666666666668,
+      },
+    },
   ])(
     `Should know that for the numbers $numbers the expectedValues are $expectedValues`,
     ({ numbers, expectedValues }) => {
