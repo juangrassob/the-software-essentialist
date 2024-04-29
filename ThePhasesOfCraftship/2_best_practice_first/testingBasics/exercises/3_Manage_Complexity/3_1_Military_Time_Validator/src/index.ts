b021b49f63c47972e5468bc7d06a67b83a7d9e30
Juan Grasso <juangrassob@gmail.com>
2024-04-29 18:13:15 -0300

    [Refactor] Deleted duplication in implementation

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,10 +1,12 @@
 export class MilitaryTimeValidator {
   static validate(time: string): boolean {
-    const validTimeRangeFormat =
-      time.length === 13 && time.split(" - ").length === 2;
+    const splitTimeRange = time.split(" - ");
 
-    const initialTime = time.split(" - ")[0];
-    const finalTime = time.split(" - ")[1];
+    const validTimeRangeFormat =
+      time.length === 13 && splitTimeRange.length === 2;
+
+    const initialTime = splitTimeRange[0];
+    const finalTime = splitTimeRange[1];
 
     const validTimeFormat =
       initialTime.split(":").length === 2 && finalTime.split(":").length === 2;
