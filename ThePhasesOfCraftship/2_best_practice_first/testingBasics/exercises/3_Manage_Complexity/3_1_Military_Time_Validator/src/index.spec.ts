0051e8e3a202fd67ba303e3ecd2c8d461dcac2bb
Juan Grasso <juangrassob@gmail.com>
2024-04-29 16:27:50 -0300

    It have a valid response format: Returns a boolean.

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,5 +1,8 @@
+import { MilitaryTimeValidator } from "./index";
 
-describe('military time validator', () => {
-
-
-})
+describe("military time validator", () => {
+  it("Should have a valid response format", () => {
+    const valid: boolean = MilitaryTimeValidator.validate("22:01 - 23:23");
+    expect(typeof valid).toBe("boolean");
+  });
+});
