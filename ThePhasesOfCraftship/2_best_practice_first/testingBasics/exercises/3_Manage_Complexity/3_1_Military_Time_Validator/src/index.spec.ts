34eebaf5e57c1eaef04b616a976766a701ada32b
Juan Grasso <juangrassob@gmail.com>
2024-04-29 17:12:57 -0300

    It know when a time range hava valid format.

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -5,4 +5,11 @@ describe("military time validator", () => {
     const valid: boolean = MilitaryTimeValidator.validate("22:01 - 23:23");
     expect(typeof valid).toBe("boolean");
   });
+
+  describe("Should know if a time range have correct format", () => {
+    it("Should know that '01:12 - 14:32' is valid", () => {
+      const valid = MilitaryTimeValidator.validate("01:12 - 14:32");
+      expect(valid).toBeTruthy();
+    });
+  });
 });
