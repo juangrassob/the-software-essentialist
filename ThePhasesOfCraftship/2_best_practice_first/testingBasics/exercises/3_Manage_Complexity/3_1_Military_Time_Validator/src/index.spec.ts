d3bbe9bbb466e128e30ba8e45acc211d1bb8171b
Juan Grasso <juangrassob@gmail.com>
2024-05-02 18:49:11 -0300

    [Refactor] Abstracted and rearanged functionality

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,58 +1,60 @@
-import { MilitaryTimeValidator } from "./index";
+import { MilitaryTimeRangeValidator } from "./index";
 
 describe("military time validator", () => {
   describe("Should know if a time range is correct", () => {
     it("Should know that the time range 12:33 - 15:52 is valid", () => {
-      const valid = MilitaryTimeValidator.validate("12:33 - 15:52");
+      const valid = MilitaryTimeRangeValidator.validate("12:33 - 15:52");
       expect(valid).toBeTruthy();
     });
 
     describe("Should know if a time range have incorrect format", () => {
       it("Should know that the time range 1:12-12:32 is not valid", () => {
-        const valid = MilitaryTimeValidator.validate("1:12-12:32");
+        const valid = MilitaryTimeRangeValidator.validate("1:12-12:32");
         expect(valid).toBeFalsy();
       });
 
       it("Should know that the time range 1:12 - 12:32 - 14:44 is not valid", () => {
-        const valid = MilitaryTimeValidator.validate("1:12 - 12:32 - 14:44");
+        const valid = MilitaryTimeRangeValidator.validate(
+          "1:12 - 12:32 - 14:44"
+        );
         expect(valid).toBeFalsy();
       });
     });
 
     describe("Should know if a military time have incorrect format", () => {
       it("Should know that the military time 1.12 - 12:32 is not valid", () => {
-        const valid = MilitaryTimeValidator.validate("1.12 - 12:32");
+        const valid = MilitaryTimeRangeValidator.validate("1.12 - 12:32");
         expect(valid).toBeFalsy();
       });
       it("Should know that the military time 1:12 - 12.32 is not valid", () => {
-        const valid = MilitaryTimeValidator.validate("1:12 - 12.32");
+        const valid = MilitaryTimeRangeValidator.validate("1:12 - 12.32");
         expect(valid).toBeFalsy();
       });
     });
 
     describe("Should know if a military time hour or minute is out of range", () => {
       it("Should know that the time range 27:32 - 12:45 is not valid", () => {
-        const valid = MilitaryTimeValidator.validate("27:32 - 12:54");
+        const valid = MilitaryTimeRangeValidator.validate("27:32 - 12:54");
         expect(valid).toBeFalsy();
       });
       it("Should know that the time range 22:123 - 12:45 is not valid", () => {
-        const valid = MilitaryTimeValidator.validate("22:123 - 12:54");
+        const valid = MilitaryTimeRangeValidator.validate("22:123 - 12:54");
         expect(valid).toBeFalsy();
       });
       it("Should know that the time range 22.23:12 - 12:45 is not valid", () => {
-        const valid = MilitaryTimeValidator.validate("22.23:12 - 12:54");
+        const valid = MilitaryTimeRangeValidator.validate("22.23:12 - 12:54");
         expect(valid).toBeFalsy();
       });
       it("Should know that the time range 22:12.11 - 12:45 is not valid", () => {
-        const valid = MilitaryTimeValidator.validate("22:12.11 - 12:54");
+        const valid = MilitaryTimeRangeValidator.validate("22:12.11 - 12:54");
         expect(valid).toBeFalsy();
       });
       it("Should know that the time range 2I:12 - 12:45 is not valid", () => {
-        const valid = MilitaryTimeValidator.validate("2I:12 - 12:54");
+        const valid = MilitaryTimeRangeValidator.validate("2I:12 - 12:54");
         expect(valid).toBeFalsy();
       });
       it("Should know that the time range 20:2P - 12:45 is not valid", () => {
-        const valid = MilitaryTimeValidator.validate("20:2P - 12:54");
+        const valid = MilitaryTimeRangeValidator.validate("20:2P - 12:54");
         expect(valid).toBeFalsy();
       });
     });
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,35 +1,74 @@
-export class MilitaryTimeValidator {
-  private static validateMilitaryTime(militaryTime: string): boolean {
-    const parsedMilitaryTime = militaryTime.split(":");
+type ParsedTimeRangeResult = {
+  startTime: string;
+  endTime: string;
+};
 
-    if (parsedMilitaryTime.length !== 2) return false;
+type ParseMilitaryTimeResult = {
+  hour: string;
+  minutes: string;
+};
 
-    const [hour, minutes] = parsedMilitaryTime;
-
-    if (/[^0-9]/.test(hour)) return false;
-    if (/[^0-9]/.test(minutes)) return false;
-
-    const parsedHour = Number.parseFloat(hour);
-    const parsedMinutes = Number.parseFloat(minutes);
-
-    return (
-      parsedHour <= 24 &&
-      parsedHour >= 0 &&
-      parsedMinutes <= 59 &&
-      parsedMinutes >= 0
-    );
-  }
-
+export class MilitaryTimeRangeValidator {
   static validate(timeRange: string): boolean {
-    const parsedTimeRange = timeRange.split(" - ");
+    const validTimeRange = this.validateTimeRange(timeRange);
 
-    if (parsedTimeRange.length !== 2) return false;
+    if (!validTimeRange) return false;
 
-    const [startTime, endTime] = parsedTimeRange;
+    const { startTime, endTime } = this.parseTimeRange(timeRange);
 
     const validStartTime = this.validateMilitaryTime(startTime);
     const validEndTime = this.validateMilitaryTime(endTime);
 
-    return validStartTime && validEndTime;
+    if (!validStartTime || !validEndTime) return false;
+
+    return true;
+  }
+
+  private static validateTimeRange(timeRange: string): boolean {
+    return timeRange.split(" - ").length === 2;
+  }
+
+  private static parseTimeRange(timeRange: string): ParsedTimeRangeResult {
+    const [startTime, endTime] = timeRange.split(" - ");
+
+    return { startTime, endTime };
+  }
+
+  private static validateMilitaryTime(militaryTime: string): boolean {
+    const validMilitaryTime = this.validateMilitaryTimeFormat(militaryTime);
+
+    if (!validMilitaryTime) return false;
+
+    const { hour, minutes } = this.parseMilitaryTime(militaryTime);
+
+    const validHour = this.validateHour(hour);
+    const validMinutes = this.validateMinutes(minutes);
+
+    return validHour && validMinutes;
+  }
+
+  private static parseMilitaryTime(
+    militaryTime: string
+  ): ParseMilitaryTimeResult {
+    const [hour, minutes] = militaryTime.split(":");
+    return { hour, minutes };
+  }
+
+  private static validateMilitaryTimeFormat(militaryTime: string): boolean {
+    return militaryTime.length !== 2;
+  }
+
+  private static validateHour(hour: string): boolean {
+    if (/[^0-9]/.test(hour)) return false;
+
+    const parsedHour = Number.parseInt(hour);
+
+    return parsedHour <= 24 && parsedHour >= 0;
+  }
+
+  private static validateMinutes(minutes: string): boolean {
+    if (/[^0-9]/.test(minutes)) return false;
+    const parsedMinutes = Number.parseInt(minutes);
+    return parsedMinutes <= 59 && parsedMinutes >= 0;
   }
 }
