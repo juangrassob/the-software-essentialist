4a20e02ef35abfb7b606326cbf66970cd85e38f5
Juan Grasso <juangrassob@gmail.com>
2024-05-02 22:18:55 -0300

    Added assgiment required test cases

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -2,13 +2,17 @@ import { MilitaryTimeRangeValidator } from "./index";
 
 describe("military time validator", () => {
   describe("Should know if a time range is correct", () => {
-    it.each(["12:33 - 15:52", "23:32 - 23:56", "1:22 - 12:33", "00:33 - 9:22"])(
-      "Should know that the time range %s is valid",
-      (timeRange) => {
-        const valid = MilitaryTimeRangeValidator.validate(timeRange);
-        expect(valid).toBeTruthy();
-      }
-    );
+    it.each([
+      "12:33 - 15:52",
+      "23:32 - 23:56",
+      "1:22 - 12:33",
+      "00:33 - 9:22",
+      "01:12 - 14:32",
+      "22:00 - 23:12",
+    ])("Should know that the time range %s is valid", (timeRange) => {
+      const valid = MilitaryTimeRangeValidator.validate(timeRange);
+      expect(valid).toBeTruthy();
+    });
 
     describe("Should know if a time range have incorrect format", () => {
       it.each(["1:12-12:32", "1:12 - 12:33 - 14:44", "22:33 ~ 23:10"])(
@@ -38,6 +42,7 @@ describe("military time validator", () => {
         "22:12.11 - 12:45",
         "22.23:12 - 12:45",
         "20:2P - 12:54",
+        "25:00 - 12:23",
       ])("Should know that the time range %s is not valid", (timeRange) => {
         const valid = MilitaryTimeRangeValidator.validate(timeRange);
         expect(valid).toBeFalsy();
